c8136bfdfbdce5d18652706d3902ea5dbe3490de
Trevor Yu <trevor.c.yu@gmail.com>
2023-03-30 03:30:25 +0000

    Update augment class to shift outputs by 1

--- utils/dataset.py
+++ utils/dataset.py
@@ -25,20 +25,20 @@ class SmartwatchDataset(torch.utils.data.Dataset):
         return len(self.data)
 
     def __getitem__(self, index):
-        """Returns tuple of (data, label) at index"""
+        """Returns tuple of (imu, mocap) at index"""
         item = self.data[index]
-        inputs = item[:, 0:9]  # IMU sensor data [accel, mag, gyro]
-        labels = item[:, 9:]  # Mocap data [pos, quat]
-        return inputs, labels
+        imu = item[:, 0:9]  # IMU sensor data [accel, mag, gyro]
+        mocap = item[:, 9:]  # Mocap data [pos, quat]
+        return imu, mocap
     
 
-class SmartwatchAugment:
+class SmartwatchAugmentLstm:
     """
     Collate function to apply random augmentations to the data
         - Randomly perturb the mocap positions
         - Randomly flip sign of mocap quaternion
         - Add random noise to IMU channels
-        - Random crop of the signal window, if possible
+        - Random crop to the signal (if possible)
     """
     def __init__(self, position_noise=0.2, accel_eps=0.01, gyro_eps=0.01, mag_eps=0.01, max_samples=512):
         """
@@ -55,66 +55,79 @@ class SmartwatchAugment:
         self.mag_eps = mag_eps
         self.max_samples = max_samples
 
-    def _random_crop(self, inputs, labels):
-        """Apply a random crop of the signal of length self.max_samples to both inputs and labels, if able to"""
-        n, d = inputs.shape
-        max_offset = n - self.max_samples
+    def _random_crop(self, imu, mocap):
+        """
+        Apply a random crop of the signal of length self.max_samples to both inputs and labels, if able to
+        Due to targets being a shifted version of decoder inputs, we need to account for one extra timepoint
+        """
+        n, d = imu.shape
+        max_offset = n - self.max_samples - 1
 
         if max_offset > 0:
             offset = rng.choice(max_offset)
-            inds = slice(offset, offset + self.max_samples)
-            return inputs[inds, :], labels[inds, :]
+            inds = slice(offset, offset + self.max_samples + 1)
+            return imu[inds, :], mocap[inds, :]
         else:
-            return inputs, labels
+            return imu, mocap
 
     def __call__(self, data):
         """
         Parameters:
         -----------
-        data: list of tuple of (inputs, labels) of length batch_size
-            inputs: np.ndarray, dimensions (n_samples, 9), signal data for IMU accel, gyro, and mag
-            labels: np.ndarray, dimensions (n_samples, 7), position and quaternion data from mocap
+        data: list of tuple of (imu, mocap) of length batch_size
+            imu: np.ndarray, dimensions (n_samples, 9), signal data for IMU accel, gyro, and mag
+            mocap: np.ndarray, dimensions (n_samples, 7), position and quaternion data from mocap
 
         Returns:
         --------
-        (inputs, labels): augmented signal data, augmented labels
+        collated_data: dict of torch.nn.utils.rnn.PackedSequence with keys ["encoder_inputs", "decoder_inputs", "targets"]
         """
-        x = []
-        y = []
-        for (inputs, labels) in data:
-            inputs, labels = self._random_crop(inputs, labels)
+        encoder_inputs = []
+        decoder_inputs = []
+        targets = []
+        for (imu, mocap) in data:
+            imu, mocap = self._random_crop(imu, mocap)
 
-            n_in, d_in = inputs.shape
-            n_out, d_out = labels.shape
-            assert n_in == n_out, "Inputs and outputs must have the same number of sequence elements"
-            assert d_in == 9, f"Input has dimensionality {d_in} instead of 9"
-            assert d_out == 7, f"Output has dimensionality {d_out} instead of 7"
+            n_in, d_in = imu.shape
+            n_out, d_out = mocap.shape
+            assert n_in == n_out, "IMU and mocap must have the same number of sequence elements"
+            assert d_in == 9, f"IMU data has dimensionality {d_in} instead of 9"
+            assert d_out == 7, f"Mocap data has dimensionality {d_out} instead of 7"
 
             # Augment XYZ positions
             offset = rng.uniform(-self.position_noise, self.position_noise, size=(1, 3))
-            labels[:, 0:3] += offset
+            mocap[:, 0:3] += offset
             # Augment quaternion sign
             sign = rng.choice([-1, 1])
-            labels[:, 4:] *= sign
+            mocap[:, 4:] *= sign
 
             accel_noise = rng.normal(loc=0, scale=self.accel_eps, size=(n_in, 3))
             gyro_noise = rng.normal(loc=0, scale=self.gyro_eps, size=(n_in, 3))
             mag_noise = rng.normal(loc=0, scale=self.mag_eps, size=(n_in, 3))
 
             noise = np.hstack([accel_noise, gyro_noise, mag_noise])
-            inputs += noise
+            imu += noise
 
-            x.append(torch.FloatTensor(inputs))
-            y.append(torch.FloatTensor(labels))
-        lengths = [len(item) for item in x]
-        inds = np.flip(np.argsort(lengths)).copy() # PackedSequence expects lengths from longest to shortest
+            # Ensure targets are one timestep shifted wrt inputs
+            encoder_inputs.append(torch.FloatTensor(imu[:-1, :]))
+            decoder_inputs.append(torch.FloatTensor(mocap[:-1, :]))
+            targets.append(torch.FloatTensor(mocap[1:, :]))
+
+        lengths = [len(item) for item in encoder_inputs]
+        inds = np.flip(np.argsort(lengths)).copy()  # PackedSequence expects lengths from longest to shortest
         lengths = torch.LongTensor(lengths)[inds]
 
         # Sort by lengths
-        x = [x[i] for i in inds]
-        y = [y[i] for i in inds]
+        encoder_inputs = [encoder_inputs[i] for i in inds]
+        decoder_inputs = [decoder_inputs[i] for i in inds]
+        targets = [targets[i] for i in inds]
 
-        packed_inputs = torch.nn.utils.rnn.pack_sequence(x)
-        packed_labels = torch.nn.utils.rnn.pack_sequence(y)
-
-        return packed_inputs, packed_labels
+        encoder_inputs = torch.nn.utils.rnn.pack_sequence(encoder_inputs)
+        decoder_inputs = torch.nn.utils.rnn.pack_sequence(decoder_inputs)
+        targets = torch.nn.utils.rnn.pack_sequence(targets)
+        collated_data = {
+            "encoder_inputs": encoder_inputs,
+            "decoder_inputs": decoder_inputs,
+            "targets": targets
+        }
+        return collated_data
